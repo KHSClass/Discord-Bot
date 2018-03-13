4fb12b77845cf203a94c68df409aa332f78550d7
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2018-03-13 08:25:31 -0400

    new framework that would be used

--- code/bot.js
+++ code/bot.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
-const Commando = require('discord.js-commando'),
+const Commando = require('wonderwork'),
 const Manager = new Discord.ShardingManager('./index.js');
 Manager.spawn(2)
